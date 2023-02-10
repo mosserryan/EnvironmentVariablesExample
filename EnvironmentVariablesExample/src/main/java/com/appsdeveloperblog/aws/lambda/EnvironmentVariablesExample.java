0f57e6f0690964945dcace5a4efb356584b6b683
mosserryan <mosser.ryan.a@gmail.com>
2023-02-10 12:32:27 -0800

    Added decryption of env key to test functionality

--- EnvironmentVariablesExample/src/main/java/com/appsdeveloperblog/aws/lambda/EnvironmentVariablesExample.java
+++ EnvironmentVariablesExample/src/main/java/com/appsdeveloperblog/aws/lambda/EnvironmentVariablesExample.java
@@ -1,17 +1,30 @@
 package com.appsdeveloperblog.aws.lambda;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DecryptRequest;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.util.Base64;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class EnvironmentVariablesExample implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final String MY_VARIABLE = decryptKey("MY_VARIABLE");
+    private static final String MY_COGNITO_USER_POOL_ID = decryptKey("MY_COGNITO_USER_POOL_ID");
+    private static final String MY_COGNITO_CLIENT_APP_SECRET = decryptKey("MY_COGNITO_CLIENT_APP_SECRET");
+    private static final String MY_GLOBAL_VARIABLE = decryptKey("MY_GLOBAL_VARIABLE");
+
 
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
         Map<String, String> headers = new HashMap<>();
@@ -20,9 +33,33 @@ public class EnvironmentVariablesExample implements RequestHandler<APIGatewayPro
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
 
+        // NEVER LOG KEYS, THIS IS FOR TRAINING PURPOSES ONLY
+        LambdaLogger logger = context.getLogger();
+        logger.log("MY_VARIABLE = " + MY_VARIABLE);
+        logger.log("MY_COGNITO_USER_POOL_ID = " + MY_COGNITO_USER_POOL_ID);
+        logger.log("MY_COGNITO_CLIENT_APP_SECRET = " + MY_COGNITO_CLIENT_APP_SECRET);
+        logger.log("MY_GLOBAL_VARIABLE = " + MY_GLOBAL_VARIABLE);
+
         return response
                 .withBody("{}")
                 .withStatusCode(500);
-
     }
+
+    private static String decryptKey(String name) {
+        System.out.println("Decrypting key");
+        byte[] encryptedKey = Base64.decode(System.getenv(name));
+        Map<String, String> encryptionContext = new HashMap<>();
+        encryptionContext.put("LambdaFunctionName",
+                System.getenv("AWS_LAMBDA_FUNCTION_NAME"));
+
+        AWSKMS client = AWSKMSClientBuilder.defaultClient();
+
+        DecryptRequest request = new DecryptRequest()
+                .withCiphertextBlob(ByteBuffer.wrap(encryptedKey))
+                .withEncryptionContext(encryptionContext);
+
+        ByteBuffer plainTextKey = client.decrypt(request).getPlaintext();
+        return new String(plainTextKey.array(), Charset.forName("UTF-8"));
+    }
+
 }
